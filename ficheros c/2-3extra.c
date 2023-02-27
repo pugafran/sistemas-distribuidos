b7fc4373c679078945fea8c8cab0e9bd2338bc6b
pugafran <franciscplgi36@gmail.com>
2023-02-27 15:41:08 +0100

    27-02-2023

--- ficheros c/2-3extra.c
+++ ficheros c/2-3extra.c
@@ -1,58 +1,80 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <signal.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_PROCESOS 5 // Número máximo de procesos simultáneos permitidos
+#define MAX_PROCESOS_SIMULTANEOS 2
 
-int contador_procesos = 0;
-
-void handler(int sig) {
-    int status;
-    pid_t pid;
-    
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-        printf("Proceso hijo %d finalizado.\n", pid);
-        // Decrementar el contador de procesos activos
-        --contador_procesos;
-    }
+void atrapa_sigchld(int s){
+    wait(0);
+    return;
 }
 
-int main() {
-    contador_procesos = MAX_PROCESOS;
+int main(){
+    int sock_pasivo, sock_datos;
+    struct sockaddr_in d_local;
+    char buffer[100];
+    int leidos;
     pid_t pid;
+    int contador_procesos = 0;
 
-    // Registrar el handler para la señal SIGCHLD
-    signal(SIGCHLD, handler);
+    signal(SIGCHLD, atrapa_sigchld);
 
-    while (1) {
-        // Esperar a que llegue un cliente
-        // ...
+    sock_pasivo = socket(PF_INET, SOCK_STREAM, 0);
 
-        // Si el contador de procesos activos es mayor a cero
-        if (contador_procesos > 0) {
-            // Lanzar un proceso hijo
-            pid = fork();
+    d_local.sin_family = AF_INET;
 
-            if (pid < 0) {
-                perror("Error en fork()");
-                exit(EXIT_FAILURE);
-            } else if (pid == 0) {
-                // Código del proceso hijo
-                // ...
-                exit(EXIT_SUCCESS);
-            } else {
-                // Código del proceso padre
-                // Decrementar el contador de procesos activos
-                --contador_procesos;
+    d_local.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    d_local.sin_port = htons(7890);
+
+    bind(sock_pasivo, (struct sockaddr *)&d_local, sizeof(d_local));
+
+    listen(sock_pasivo, SOMAXCONN);
+
+    while(1){
+        if (contador_procesos < MAX_PROCESOS_SIMULTANEOS) {
+            sock_datos = accept(sock_pasivo, 0, 0);
+
+            if ((pid = fork()) < 0){
+                perror("Error en fork");
+                exit(1);
             }
-        } else {
-            // Esperar a que alguno de los procesos hijos finalice
-            // ...
+
+            else if(pid == 0){
+                printf("Soy el proceso hijo (%d) antes del while\n", getpid());
+
+                while ((leidos = read(sock_datos, buffer, sizeof(buffer))) > 0){
+                    printf("Soy el proceso hijo (%d) antes del write\n", getpid());
+                    write(sock_datos, buffer, leidos);
+                    printf("Soy el proceso hijo (%d) después del write\n", getpid());
+                }
+
+                close(sock_datos);
+
+                printf("Soy el proceso hijo (%d) antes de terminar\n", getpid());
+                exit(0);
+            }
+
+            else {
+                printf("Soy el proceso padre (%d) antes de incrementar el contador\n", getpid());
+                contador_procesos++;
+                printf("Contador de procesos: %d\n", contador_procesos);
+                close(sock_datos);
+            }
+        }
+
+        else {
+            printf("[Número máximo de procesos simultáneos alcanzados]\n");
+            printf("Soy el proceso padre (%d) esperando a que algún hijo termine\n", getpid());
+            wait(0);
+            contador_procesos--;
+            printf("Contador de procesos: %d\n", contador_procesos);
         }
     }
-
-    return 0;
 }
-
